b82fc3b088b7ad9f471c32993067cd626126489d
Andrew Watson <aww@awwsmm.com>
2020-05-23 16:41:28 +0100

    working in dummy project

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@
 //   https://adrianhall.github.io/web/2018/07/04/run-typescript-mocha-tests-in-vscode/
 
 // property-based testing:
-//   
+//   https://dubzzz.github.io/fast-check.github.com/
+
+// publishing to npm
+//   https://medium.com/the-andela-way/build-and-publish-your-first-npm-package-a4daf0e2431
+//   https://medium.com/cameron-nokes/the-30-second-guide-to-publishing-a-typescript-package-to-npm-89d93ff7bccd
 
 export { UInt32 } from './UInt32'
 export { UTF32Char } from './UTF32Char'
