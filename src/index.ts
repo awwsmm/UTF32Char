7ca7daa45b328b159cca2a46cb8421247a0ba30d
Andrew Watson <aww@awwsmm.com>
2020-05-24 21:45:24 +0100

    actually compiled the code

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@
 // publishing to npm
 //   https://medium.com/the-andela-way/build-and-publish-your-first-npm-package-a4daf0e2431
 //   https://medium.com/cameron-nokes/the-30-second-guide-to-publishing-a-typescript-package-to-npm-89d93ff7bccd
+//   https://docs.npmjs.com/updating-your-published-package-version-number
 
 export { UInt32 } from './UInt32'
 export { UTF32Char } from './UTF32Char'
